acf9766944794d835c4f68aa438a205b4341736f
ClaribelGg0 <87459826+ClaribelGg0@users.noreply.github.com>
2021-07-15 00:08:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cuerpo', {delay: 500});
+ScrollReveal().reveal('.rebajas', {delay: 500});
+ScrollReveal().reveal('.banner', {delay: 500});
+ScrollReveal().reveal('.RedSocial', {delay: 500});
+ScrollReveal().reveal('.footer-links', {delay: 500});
